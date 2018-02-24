cc14f3c51c5abd6224502fe787a4f74c44828390
atsushi130 <atsushi_main@outlook.jp>
2018-02-25 01:34:09 +0900

    implement: get attendances by month

--- src/data/attendance/attendance_repository.rs
+++ src/data/attendance/attendance_repository.rs
@@ -1,6 +1,9 @@
 
 use super::AttendanceEntity;
 use database::DatabaseConnection;
+use extension::ToTwoDigits;
+use chrono::{ Local, Datelike, DateTime, Duration, Weekday, TimeZone };
+use chrono::naive::{ NaiveDate, NaiveDateTime };
 use schema::attendances::dsl::*;
 use diesel::prelude::*;
 
@@ -26,9 +29,17 @@ impl AttendanceRepository {
         attendances.filter(user.eq(user_name)).load::<AttendanceEntity>(&*self.connection).expect("Error")
     }
 
-    // pub fn get_attendances_filterd_check_at(&self, check_at: &str) -> Vec<AttendanceEntity> {
-    //     attendances.filter(check_at.gt(date(now + 1.month()))).load::<AttendanceEntity>(&*self.connection).expect("Error")
-    // }
+    pub fn get_attendances_by_month(&self, user_name: &str, month: u32) -> Vec<AttendanceEntity> {
+
+        let year = Local::now().year();
+        let to_date = Local.ymd(year, month + 1, 1).and_hms(23, 59, 59) - Duration::days(1);
+
+        attendances
+            .filter(user.eq(user_name))
+            .filter(check_at.ge(format!("{}/{}/01 00:00:00", year, month.to_two_digits())))
+            .filter(check_at.lt(format!("{}/{}/{} 23:59:59", year, to_date.month().to_two_digits(), to_date.day())))
+            .load::<AttendanceEntity>(&*self.connection).expect("Error")
+    }
 
     pub fn register(&self, entity: &AttendanceEntity) {
     }

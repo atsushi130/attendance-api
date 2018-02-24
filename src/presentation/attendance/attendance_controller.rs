bba4c9d9a19d2182750c2b51509c63712f24776d
atsushi130 <atsushi_main@outlook.jp>
2018-02-24 23:41:02 +0900

    implement: connection injection

--- src/presentation/attendance/attendance_controller.rs
+++ src/presentation/attendance/attendance_controller.rs
@@ -4,15 +4,17 @@
 #![plugin(rocket_codegen)]
 
 use rocket_contrib::Json;
+use rocket::request::FromRequest;
 use super::AttendanceResource;
+use database::DatabaseConnection;
 
 #[post("/attendances", format="application/json", data="<resource>")]
-pub fn post_attendance(resource: Json<AttendanceResource>) -> Json<AttendanceResource> {
+pub fn post_attendance(connection: DatabaseConnection, resource: Json<AttendanceResource>) -> Json<AttendanceResource> {
     resource
 }
 
 #[get("/attendances")]
-pub fn get_attendances() -> Json<Vec<AttendanceResource>> {
+pub fn get_attendances(connection: DatabaseConnection) -> Json<Vec<AttendanceResource>> {
 
     let numbers: Vec<i32> = vec![1, 2, 3, 4, 5];
     let attendances = numbers.iter().map(|number| {
@@ -23,7 +25,7 @@ pub fn get_attendances() -> Json<Vec<AttendanceResource>> {
 }
 
 #[get("/attendances/<user>")]
-pub fn get_attendances_filterd_user(user: String) -> Json</*Vec<*/AttendanceResource/*>*/> {
+pub fn get_attendances_filterd_user(connection: DatabaseConnection, user: String) -> Json</*Vec<*/AttendanceResource/*>*/> {
     let attendance = AttendanceResource::new("atsushi", "2018/02/24 12:00:00:00", 0);
     Json(attendance)
 }

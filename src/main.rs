bb2adf84381bb07e55208152b5b431479788c576
atsushi130 <atsushi_main@outlook.jp>
2018-02-25 16:20:26 +0900

    add: macro use attribute to rocket_contrib

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
+#[macro_use]
 extern crate rocket_contrib;
 
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
 
 #[macro_use]
 extern crate diesel;
+
+extern crate rocket;
+extern crate serde;
 extern crate dotenv;
 extern crate r2d2;
 extern crate r2d2_diesel;

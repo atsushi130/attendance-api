e7d20728c7cc84990a33a9bec64f0d75d9c88843
atsushi130 <atsushi_main@outlook.jp>
2018-02-25 16:18:13 +0900

    add: insertable attendance entity

--- src/data/attendance/attendance_entity.rs
+++ src/data/attendance/attendance_entity.rs
@@ -1,8 +1,28 @@
 
+use schema::attendances;
+
 #[derive(Queryable)]
 pub struct AttendanceEntity {
     pub id: i32,
     pub user: String,
     pub check_at: String,
     pub attendance_type: i32
+}
+
+#[derive(Insertable)]
+#[table_name = "attendances"]
+pub struct AttendanceInsertableEntity {
+    pub user: String,
+    pub check_at: String,
+    pub attendance_type: i32
+}
+
+impl AttendanceInsertableEntity {
+    pub fn new(user: &str, check_at: &str, attendance_type: i32) -> Self {
+        AttendanceInsertableEntity {
+            user: user.to_string(),
+            check_at: check_at.to_string(),
+            attendance_type
+        }
+    }
 }

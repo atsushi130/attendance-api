6fb26ce5f4aec612c5debc88c490eaaa8663e37d
atsushi130 <atsushi_main@outlook.jp>
2018-03-01 22:16:32 +0900

    implement: create date time method

--- src/domain/attendance/attendance_model.rs
+++ src/domain/attendance/attendance_model.rs
@@ -1,4 +1,6 @@
 
+use chrono::{ DateTime, Local };
+
 pub struct AttendanceModel {
     pub user: String,
     pub check_at: String,
@@ -6,11 +8,16 @@ pub struct AttendanceModel {
 }
 
 impl AttendanceModel {
+
     pub fn new(user: &str, check_at: &str, attendance_type: i32) -> Self {
         AttendanceModel {
             user: user.to_string(),
             check_at: check_at.to_string(),
             attendance_type
         }
+    }
+
+    pub fn get_check_at_date(&self) -> DateTime<Local> {
+        DateTime::from_str(&self.check_at, "%Y/%m/%d %H:%M:%S").unwrap()
     }
 }

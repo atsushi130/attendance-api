c53b0f81988741d2cec8a8dc30c29052791e67e9
atsushi130 <atsushi_main@outlook.jp>
2018-03-04 01:48:38 +0900

    implement: get working time method

--- src/domain/attendance/attendance_model.rs
+++ src/domain/attendance/attendance_model.rs
@@ -1,5 +1,7 @@
 
-use chrono::{ DateTime, Local };
+use chrono::{ DateTime, Local, TimeZone, Duration, Timelike, Datelike };
+use std::str::FromStr;
+use std::fmt::Debug;
 
 pub struct AttendanceModel {
     pub user: String,
@@ -18,6 +20,59 @@ impl AttendanceModel {
     }
 
     pub fn get_check_at_date(&self) -> DateTime<Local> {
-        DateTime::from_str(&self.check_at, "%Y/%m/%d %H:%M:%S").unwrap()
+        Local.datetime_from_str(&self.check_at, "%Y/%m/%d %H:%M:%S").unwrap()
+    }
+
+    pub fn get_working_time(&self, attendance: &AttendanceModel) -> f32 {
+
+        if !self.validate(attendance) {
+            return 0.0
+        }
+
+        let from = self.get_check_at_date();
+        let to   = attendance.get_check_at_date();
+
+
+        let duration = self.get_duration();
+        let date_time = to - duration;
+
+        println!("{}", date_time.to_string());
+
+        (date_time.hour() as f32) + (date_time.minute() as f32 / 60.0)
+    }
+
+    fn validate(&self, attendance: &AttendanceModel) -> bool {
+
+        if !self.is_valid_working(attendance) {
+            return false
+        }
+
+        if !self.is_equal_day(attendance) {
+            return false
+        }
+
+        true
+    }
+
+    fn is_equal_day(&self, attendance: &AttendanceModel) -> bool {
+
+        let from = self.get_check_at_date();
+        let to   = attendance.get_check_at_date();
+
+        let year  = from.year()  == to.year();
+        let month = from.month() == to.month();
+        let day   = from.day()   == to.day();
+
+        year && month && day
+    }
+
+    fn is_valid_working(&self, attendance: &AttendanceModel) -> bool {
+        (self.attendance_type == 0) && (attendance.attendance_type == 1)
+    }
+
+    fn get_duration(&self) -> Duration {
+        let check_at = self.get_check_at_date();
+        println!("duration check at: {}", check_at.to_string());
+        (Duration::hours(check_at.hour() as i64) + Duration::minutes(check_at.minute() as i64) + Duration::seconds(check_at.second() as i64))
     }
 }
